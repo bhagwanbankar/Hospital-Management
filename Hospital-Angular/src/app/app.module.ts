66adbb39ee1b8aaf508411edf4d0bf32551838ec
bhagwanbankar <bhagwan.bankar312@gmail.com>
2019-10-17 23:51:35 +0530

    angular material added

--- Hospital-Angular/src/app/app.module.ts
+++ Hospital-Angular/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PatientsComponent } from './patients/patients.component';
+import { PatientComponent } from './patients/patient/patient.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientsComponent,
+    PatientComponent
   ],
   imports: [
     BrowserModule,
